15681d02547981d71e027ca58d241b549e62153e
Zecharias Mengis <zmengis@gmail.com>
2021-05-25 18:31:06 -0500

    Done.

--- src/Matrix.js
+++ src/Matrix.js
@@ -1,12 +1,15 @@
 import React, { Component } from 'react';
+import Cell from './cell.js'
 
 export default class Matrix extends Component {
   
   genRow = (vals) => {
-    return vals.map(val => <div className="cell"></div>) // replace me and render a cell component instead!
+    // console.log(vals)
+    return vals.map( val => <Cell value={val} />)
   }
   
   genMatrix = () => {
+    console.log(this.props.values)
     return this.props.values.map(rowVals => <div className="row">{this.genRow(rowVals)}</div>)
   }
   
@@ -18,4 +21,11 @@ export default class Matrix extends Component {
     )
   }
   
+}
+
+Matrix.defaultProps = {
+  values: (() => {
+    const defRow = ['#F00', '#F00', '#F00', '#F00', '#F00', '#F00', '#F00', '#F00', '#F00', '#F00']
+    return (new Array(10).fill(defRow))
+  })()
 }
---
+++ src/cell.js
@@ -0,0 +1,24 @@
+import React, { Component } from 'react';
+
+export default class Cell extends Component {
+
+    constructor(props) {
+        super();
+        this.state = {
+            color: props.value
+        }
+    }
+
+    handleClick = () => {
+        this.setState({
+          color: '#333'
+        })
+      }
+    
+    render() {
+      return (
+          <div className="cell" style={{backgroundColor: this.state.color}} onClick={this.handleClick}></div>
+      )
+    }
+    
+  }
